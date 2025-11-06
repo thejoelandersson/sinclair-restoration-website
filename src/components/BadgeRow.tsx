5e2d92f159456c3803a2667bbe93640c8bc09047
Joel Andersson <thejoelandersson@gmail.com>
2025-11-06 10:55:32 -0600

    Remove 6-Month Workmanship Warranty badge from hero page
    - Removed '6-Month Workmanship Warranty' badge from BadgeRow component
    - BadgeRow is used on the homepage hero section
    - Remaining badges: Licensed & Insured, Independent Clearance Testing, Same-day Service, Locally Owned & Operated, EPA-Compliant Procedures

--- src/components/BadgeRow.tsx
+++ src/components/BadgeRow.tsx
@@ -23,13 +23,6 @@ export default function BadgeRow({ className = '' }: BadgeRowProps) {
       
       <span className="flex items-center space-x-2">
         <svg className="w-4 h-4 text-[var(--sin-blue-700)]" fill="currentColor" viewBox="0 0 20 20">
-          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
-        </svg>
-        <span>6-Month Workmanship Warranty</span>
-      </span>
-      
-      <span className="flex items-center space-x-2">
-        <svg className="w-4 h-4 text-[var(--sin-blue-700)]" fill="currentColor" viewBox="0 0 20 20">
           <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
         </svg>
         <span>Same-day Service</span>
