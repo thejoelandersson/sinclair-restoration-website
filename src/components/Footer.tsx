d3c566099ff857a6a8a31470b178cdb58d2d014f
Joel Andersson <thejoelandersson@gmail.com>
2025-11-06 10:56:15 -0600

    Remove 6-Month Workmanship Warranty badge from Footer
    - Removed '6-Month Workmanship Warranty' badge from Footer component
    - Footer now shows: Licensed & Insured, Independent Clearance Testing
    - Matches the hero page badge removal

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,13 +30,6 @@ export default function Footer() {
                   </svg>
                   Independent Clearance Testing
                 </span>
-                <span className="hidden sm:inline text-white/50">•</span>
-                <span className="flex items-center gap-2">
-                  <svg className="w-4 h-4 text-white/90" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
-                  </svg>
-                  6-Month Workmanship Warranty
-                </span>
               </div>
             </div>
           </div>
