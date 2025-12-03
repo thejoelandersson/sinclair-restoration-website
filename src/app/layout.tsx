1ed5525c894f7ef4a1ae5919999a50666992d096
justinsinclairrestoration <Justin@sinclairrestoration.com>
2025-12-03 15:36:22 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2BLVM2HGKM">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2BLVM2HGKM');
+</script>
   {/* Google Tag Manager */}
   <script
     dangerouslySetInnerHTML={{
