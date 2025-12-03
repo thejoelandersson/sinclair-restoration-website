41603e50f734abe29a8e6ce89fe427eaffed0e59
justinsinclairrestoration <Justin@sinclairrestoration.com>
2025-12-03 15:27:52 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,35 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="en">
+      <head>
+  {/* Google Tag Manager */}
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WMHHVLMC');
+      `,
+    }}
+  />
+</head>
+
+<body>
+  {/* GTM noscript */}
+  <noscript>
+    <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-WMHHVLMC"
+      height="0"
+      width="0"
+      style={{ display: 'none', visibility: 'hidden' }}
+    />
+  </noscript>
+
+  {children}
+</body>
+
       <body className="antialiased min-h-screen">
         <script
           type="application/ld+json"
